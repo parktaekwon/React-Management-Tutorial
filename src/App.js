8f0e5b585cc1c15c60373fa938290b838375abfe
ParkTaekwon <elanvita@gmail.com>
2023-07-03 19:42:52 +0900

    컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 import { render } from '@testing-library/react';
 
+const customers = [
+ {
+  'id' : 1,
+  'image' : 'https://img.khan.co.kr/news/2023/07/03/rcv.PHOTOPOOL.20230703.kpa16883582480957_P1.webp',
+  'name' : '박태권',
+  'birthday' : '731215',
+  'gender' :'남',
+  'job' :'보험사직원'
+},
+{
+  'id' : 2,
+  'image' : 'https://img.khan.co.kr/news/2023/07/03/rcv.PHOTOPOOL.20230703.kpa16883582480957_P1.webp',
+  'name' : '윤석열',
+  'birthday' : '881111',
+  'gender' :'여',
+  'job' :'증권사직원'
+},
+{
+  'id' : 3,
+  'image' : 'https://img.khan.co.kr/news/2023/07/03/rcv.PHOTOPOOL.20230703.kpa16883582480957_P1.webp',
+  'name' : '김개동',
+  'birthday' : '001111',
+  'gender' :'게이',
+  'job' :'저축은행직원'
+}
+]
+
+
 class App extends Component {
-  render(){
+  render() {
       return (
-        <div className="gray-background">
-            <img src={logo} alt="logo" />
-            <p> Let's develop management system ~ ~~~
-            </p>
+        <div>
+          {
+            customers.map(c => {
+              return(
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              )
+            })
+          }
+        
+
         </div>
-    );
+    )
   }
 }
 
